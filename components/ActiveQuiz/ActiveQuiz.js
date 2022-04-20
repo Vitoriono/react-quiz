007e4854a48ac149f11be33c80a1dd3caeed01ac
Victor <korovin.vic@gmail.com>
2022-04-20 19:27:48 +0300

    second commit

---
+++ components/ActiveQuiz/ActiveQuiz.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import classes from './ActiveQuiz.module.css';
+import  AnswerList from './AnswersLIst/AnswersList';
+
+const ActiveQuiz = props => (
+    <div className={classes.ActiveQuiz}>
+        <p className ={classes.Question}>
+          <span>
+            <strong>2.</strong>&nbsp;
+            Как дела?
+          </span>
+
+          <small>4 из 12</small>
+        </p>
+
+            <AnswerList
+                answers={props.answers}
+             />
+
+    </div>
+)
+export default ActiveQuiz
